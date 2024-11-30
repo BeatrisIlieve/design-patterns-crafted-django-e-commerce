fe8faa713ce2a27591c45fe5846ab7d71634c5b0
Beatris Ilieve <beatrisilieve@icloud.com>
2024-11-30 15:30:27 +0200

    added app

---
+++ design_patterns_crafted_django_e_commerce/user_payment_details/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UserPaymentDetailsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "design_patterns_crafted_django_e_commerce.user_payment_details"
