98cfb13561f8ea2100a7603a50fec4c2445c8cf1
Beatris Ilieve <beatrisilieve@icloud.com>
2024-11-30 17:48:45 +0200

    added test function

--- design_patterns_crafted_django_e_commerce/product/managers.py
+++ design_patterns_crafted_django_e_commerce/product/managers.py
@@ -5,16 +5,15 @@ from django.db.models import (
     Min,
     Max,
     Sum,
-    F,
     When,
     Case,
     Value,
-    BooleanField
+    BooleanField,
 )
 
 
 class ProductManager(models.Manager):
-    
+
     def represent_entity_into_products_list(self, category_pk, color_pk):
         return (
             self.filter(category__pk=category_pk, color__pk=color_pk)
@@ -32,19 +31,6 @@ class ProductManager(models.Manager):
             )
         )
 
-    # def represent_entity_into_products_list(self, category_pk, color_pk):
-    #     return (
-    #         self.filter(category__pk=category_pk, color__pk=color_pk)
-    #         .select_related("category", "color")
-    #         .prefetch_related("product_inventory")
-    #         .annotate(
-    #             min_price=Min("product_inventory__price"),
-    #             max_price=Max("product_inventory__price"),
-    #         )
-    #     )
-
-    #         .annotate(item_total=F("quantity") * F("product__product_inventory__price"))
-    #         .aggregate(total=Sum("item_total"))["total"]
     def represent_entity_into_product_page(self, category_pk, color_pk):
         return (
             self.filter(category__pk=category_pk, color__pk=color_pk)
