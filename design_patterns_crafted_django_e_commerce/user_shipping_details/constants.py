b40ae6e8c028c0721034aa7bfd5ced1d6846e2f2
Beatris Ilieve <beatrisilieve@icloud.com>
2024-11-30 15:29:06 +0200

    added model

---
+++ design_patterns_crafted_django_e_commerce/user_shipping_details/constants.py
@@ -0,0 +1,71 @@
+FIRST_NAME_RULES = {
+    "max_length": 255,
+    "min_length": 2,
+    "pattern": "(^[A-Za-z]{2,}$)|(^[A-Za-z]{1,}[\s\-]?[A-Za-z]{1,}$)",
+    "pattern_error_message": "First Name can only contain letters, spaces, hyphens, and must start and end with a letter",
+    "null": False,
+    "blank": False,
+}
+
+LAST_NAME_RULES = {
+    "max_length": 255,
+    "min_length": 2,
+    "pattern": "(^[A-Za-z]{2,}$)|(^[A-Za-z]{1,}[\s\-]?[A-Za-z]{1,}$)",
+    "pattern_error_message": "Last Name can only contain letters, spaces, hyphens, and must start and end with a letter",
+    "null": False,
+    "blank": False,
+}
+
+PHONE_NUMBER_RULES = {
+    "max_length": 15,
+    "min_length": 7,
+    "pattern": "^[0-9]+$",
+    "pattern_error_message": "This field can only contain digits",
+    "null": False,
+    "blank": False,
+}
+
+COUNTRY_NAME_RULES = {
+    "max_length": 255,
+    "min_length": 2,
+    "pattern": "(^[A-Za-z]{2,}$)|(^[A-Za-z]{1,}[\s\-]?[A-Za-z]{1,}$)",
+    "pattern_error_message": "Country Name can only contain letters, spaces, hyphens, and must start and end with a letter",
+    "null": False,
+    "blank": False,
+}
+
+CITY_NAME_RULES = {
+    "max_length": 255,
+    "min_length": 2,
+    "pattern": "(^[A-Za-z]{2,}$)|(^[A-Za-z]{1,}[\s\-]?[A-Za-z]{1,}$)",
+    "pattern_error_message": "City Name can only contain letters, spaces, hyphens, and must start and end with a letter",
+    "null": False,
+    "blank": False,
+}
+
+STREET_ADDRESS_RULES = {
+    "max_length": 255,
+    "min_length": 8,
+    "pattern": "^([A-Za-z0-9]{1,})([A-Za-z0-9\s\-\.']{6,})([A-Za-z0-9])$",
+    "pattern_error_message": "This field can only contain letters, spaces, hyphens, apostrophes, and periods, and must start and end with a letter or digit",
+    "null": False,
+    "blank": False,
+}
+
+APARTMENT_RULES = {
+    "max_length": 10,
+    "min_length": 1,
+    "pattern": "^([A-Za-z0-9]{1,5})([A-Za-z0-9\s\-\.]{0,4})([A-Za-z0-9]?)$",
+    "pattern_error_message": "This field can only contain letters, spaces, hyphens, and periods, and must start and end with a letter or digit",
+    "null": True,
+    "blank": True,
+}
+
+POSTAL_CODE_RULES = {
+    "max_length": 15,
+    "min_length": 3,
+    "pattern": "^([A-Za-z0-9]{1,})([A-Za-z0-9\s\-\.\,]{0,12})([A-Za-z0-9]{1})$",
+    "pattern_error_message": "This field can only contain letters, spaces, hyphens, commas, and periods, and must start and end with a letter or digit",
+    "null": False,
+    "blank": False,
+}
--- design_patterns_crafted_django_e_commerce/user_shipping_details/models.py
+++ design_patterns_crafted_django_e_commerce/user_shipping_details/models.py
@@ -1,3 +1,99 @@
 from django.db import models
 
-# Create your models here.
+
+from design_patterns_crafted_django_e_commerce.common.models import (
+    BaseUserCharField,
+)
+
+from .constants import (
+    FIRST_NAME_RULES,
+    LAST_NAME_RULES,
+    PHONE_NUMBER_RULES,
+    COUNTRY_NAME_RULES,
+    CITY_NAME_RULES,
+    STREET_ADDRESS_RULES,
+    APARTMENT_RULES,
+    POSTAL_CODE_RULES,
+)
+
+
+class UserShippingDetails(BaseUserCharField):
+
+    first_name = BaseUserCharField.create_char_field(
+        max_length=FIRST_NAME_RULES["max_length"],
+        min_length=FIRST_NAME_RULES["min_length"],
+        pattern=FIRST_NAME_RULES["pattern"],
+        pattern_error_message=FIRST_NAME_RULES["pattern_error_message"],
+        null_value=FIRST_NAME_RULES["null"],
+        blank_value=FIRST_NAME_RULES["blank"],
+    )
+
+    last_name = BaseUserCharField.create_char_field(
+        max_length=LAST_NAME_RULES["max_length"],
+        min_length=LAST_NAME_RULES["min_length"],
+        pattern=LAST_NAME_RULES["pattern"],
+        pattern_error_message=LAST_NAME_RULES["pattern_error_message"],
+        null_value=LAST_NAME_RULES["null"],
+        blank_value=LAST_NAME_RULES["blank"],
+    )
+
+    phone_number = BaseUserCharField.create_char_field(
+        max_length=PHONE_NUMBER_RULES["max_length"],
+        min_length=PHONE_NUMBER_RULES["min_length"],
+        pattern=PHONE_NUMBER_RULES["pattern"],
+        pattern_error_message=PHONE_NUMBER_RULES["pattern_error_message"],
+        null_value=PHONE_NUMBER_RULES["null"],
+        blank_value=PHONE_NUMBER_RULES["blank"],
+    )
+
+    country = BaseUserCharField.create_char_field(
+        max_length=COUNTRY_NAME_RULES["max_length"],
+        min_length=COUNTRY_NAME_RULES["min_length"],
+        pattern=COUNTRY_NAME_RULES["pattern"],
+        pattern_error_message=COUNTRY_NAME_RULES["pattern_error_message"],
+        null_value=COUNTRY_NAME_RULES["null"],
+        blank_value=COUNTRY_NAME_RULES["blank"],
+    )
+
+    city = BaseUserCharField.create_char_field(
+        max_length=CITY_NAME_RULES["max_length"],
+        min_length=CITY_NAME_RULES["min_length"],
+        pattern=CITY_NAME_RULES["pattern"],
+        pattern_error_message=CITY_NAME_RULES["pattern_error_message"],
+        null_value=CITY_NAME_RULES["null"],
+        blank_value=CITY_NAME_RULES["blank"],
+    )
+
+    street_address = BaseUserCharField.create_char_field(
+        max_length=STREET_ADDRESS_RULES["max_length"],
+        min_length=STREET_ADDRESS_RULES["min_length"],
+        pattern=STREET_ADDRESS_RULES["pattern"],
+        pattern_error_message=STREET_ADDRESS_RULES["pattern_error_message"],
+        null_value=STREET_ADDRESS_RULES["null"],
+        blank_value=STREET_ADDRESS_RULES["blank"],
+    )
+
+    apartment = BaseUserCharField.create_char_field(
+        max_length=APARTMENT_RULES["max_length"],
+        min_length=APARTMENT_RULES["min_length"],
+        pattern=APARTMENT_RULES["pattern"],
+        pattern_error_message=APARTMENT_RULES["pattern_error_message"],
+        null_value=APARTMENT_RULES["null"],
+        blank_value=APARTMENT_RULES["blank"],
+    )
+
+    postal_code = BaseUserCharField.create_char_field(
+        max_length=POSTAL_CODE_RULES["max_length"],
+        min_length=POSTAL_CODE_RULES["min_length"],
+        pattern=POSTAL_CODE_RULES["pattern"],
+        pattern_error_message=POSTAL_CODE_RULES["pattern_error_message"],
+        null_value=POSTAL_CODE_RULES["null"],
+        blank_value=POSTAL_CODE_RULES["blank"],
+    )
+
+    user = models.OneToOneField(
+        to="user_credential_details.UserCredentialDetails",
+        on_delete=models.CASCADE,
+        primary_key=True,
+        related_name="shipping_details",
+    )
