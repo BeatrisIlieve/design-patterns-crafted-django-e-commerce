f3f6653266fdc07d3d901f9caab75a617a744a99
Beatris Ilieve <beatrisilieve@icloud.com>
2024-11-30 16:28:40 +0200

    added model

--- design_patterns_crafted_django_e_commerce/product/admin.py
+++ design_patterns_crafted_django_e_commerce/product/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Product,
+)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    list_filter = ("color", "category")
+
+    fieldsets = (
+        ("Details", {"fields": ("category", "color", "description")}),
+        (
+            "Images",
+            {
+                "fields": ("first_image_url", "second_image_url"),
+                "classes": ("collapse",),
+            },
+        ),
+    )
