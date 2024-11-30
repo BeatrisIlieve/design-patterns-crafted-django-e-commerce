86114e722682e5a78911fca7ed900b6d2e224644
Beatris Ilieve <beatrisilieve@icloud.com>
2024-11-30 16:27:25 +0200

    added model

--- design_patterns_crafted_django_e_commerce/inventory/models.py
+++ design_patterns_crafted_django_e_commerce/inventory/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.core.validators import (
+    MaxValueValidator,
+)
 
-# Create your models here.
+from design_patterns_crafted_django_e_commerce.product.models import (
+    Product,
+)
+
+
+class Inventory(models.Model):
+
+    class Meta:
+        unique_together = ("product", "size")
+        verbose_name_plural = "Inventories"
+
+    quantity = models.PositiveIntegerField(
+        validators=[
+            MaxValueValidator(
+                3, message="The maximum quantity per product inventory is 3."
+            ),
+        ]
+    )
+
+    product = models.ForeignKey(
+        to=Product,
+        on_delete=models.CASCADE,
+        related_name="product_inventory",
+    )
+
+    size = models.DecimalField(
+        max_digits=4,
+        decimal_places=2,
+    )
+
+    price = models.DecimalField(
+        max_digits=7,
+        decimal_places=2,
+    )
+
+    created_at = models.DateField(
+        auto_now_add=True,
+    )
+
+    updated_at = models.DateField(
+        auto_now=True,
+    )
+
+    @property
+    def is_sold_out(self):
+        return self.quantity <= 0
